e6559738919d07458a57739b7695e40009e78c35
cesar r <cesaracer@gmail.com>
2020-10-06 15:48:43 -0600

    added comments

--- src/Containers/PostList.js
+++ src/Containers/PostList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Post from '../Components/Post';
 import { connect } from 'react-redux';
 
+//container for posts
 function PostList(props){
     return(
         <div className='post-container'>
